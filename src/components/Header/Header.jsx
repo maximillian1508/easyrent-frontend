93333c95aee966e0b648d94ec06357ca07f74b1b
Maximillian Leonard <maximillian1508@gmail.com>
2024-10-04 19:25:56 +0800

    feat: header mobile responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,23 @@
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import "./Header.css";
-import { ActionIcon, Button, Divider, Menu, ThemeIcon } from "@mantine/core";
+import {
+	ActionIcon,
+	Button,
+	Divider,
+	Menu,
+	ThemeIcon,
+	Burger,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
 import { useEffect, useRef, useState } from "react";
 import { NavHashLink } from "react-router-hash-link";
 import { useSendLogoutMutation } from "../../features/auth/authApiSlice";
 import useAuth from "../../hooks/useAuth";
 
+
 const Header = () => {
+	const [opened, { toggle }] = useDisclosure();
 	const location = useLocation();
 	const [iconSrc, setIconSrc] = useState("images/er-horizontal-white.svg");
 	const [loginVariant, setLoginVariant] = useState("er-white");
@@ -18,6 +28,7 @@ const Header = () => {
 	useEffect(() => {
 		if (location.pathname === "/") {
 			setLoginVariant("er-white");
+			setIconSrc("/images/er-horizontal-white.svg");
 			const handleScroll = () => {
 				if (ref.current) {
 					const scrollTop = window.scrollY;
@@ -42,7 +53,7 @@ const Header = () => {
 		}
 		setLoginVariant("er-blue");
 
-		return () => {};
+		return () => { };
 	}, [location.pathname]);
 
 	const [sendLogout, { isError, error }] = useSendLogoutMutation();
@@ -79,7 +90,7 @@ const Header = () => {
 			className={location.pathname === "/" ? "topbar" : "topbar whitebar"}
 			ref={ref}
 		>
-			<aside className="logo-sect">
+			<aside className="logo-sect" style={{ flexShrink: 0 }}>
 				<Link to="/">
 					<img
 						src={
@@ -104,110 +115,80 @@ const Header = () => {
 						<NavHashLink to="/about-us#">About Us</NavHashLink>
 					</li>
 				</ul>
-			</nav>
-			{isCustomer ? (
-				<div className="inner-dash-topbar">
-					<ActionIcon variant="white">
-						<img
-							src="/images/notifications.svg"
-							alt="notifications"
-							width="70%"
-						/>
-					</ActionIcon>
-					<Divider orientation="vertical" size="sm" />
-					<Menu shadow="md" width={200}>
-						<Menu.Target>
-							<Button
-								variant="white"
-								color="gray"
-								leftSection={
-									<ThemeIcon variant="white" color="gray">
-										<img
-											src="/images/user-hair.svg"
-											style={{ width: "70%", height: "70%" }}
-											alt="user hair"
-										/>
-									</ThemeIcon>
-								}
-								styles={{
-									label: { overflow: "ellipsis" },
-									root: { width: "150px" },
-								}}
-							>
-								{userName}
-							</Button>
-						</Menu.Target>
+				{isCustomer ? (
+					<div className="inner-dash-topbar">
+						<ActionIcon variant="white">
+							<img
+								src="/images/notifications.svg"
+								alt="notifications"
+								width="70%"
+							/>
+						</ActionIcon>
+						<Divider orientation="vertical" size="sm" />
+						<Menu shadow="md" width={200}>
+							<Menu.Target>
+								<Button
+									variant="white"
+									color="gray"
+									leftSection={
+										<ThemeIcon variant="white" color="gray">
+											<img
+												src="/images/user-hair.svg"
+												style={{ width: "70%", height: "70%" }}
+												alt="user hair"
+											/>
+										</ThemeIcon>
+									}
+									styles={{
+										label: { overflow: "ellipsis" },
+										root: { width: "150px" },
+									}}
+								>
+									{userName}
+								</Button>
+							</Menu.Target>
+							<Menu.Dropdown>
+								<Menu.Label>Application</Menu.Label>
+								<Menu.Item
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/user.svg" alt="user" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Profile
+								</Menu.Item>
+								<Menu.Item
+									component="a"
+									href="/dashboard"
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/dashboard.svg" alt="dashboard" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Enter Dashboard
+								</Menu.Item>
+								<Menu.Divider />
 
-						<Menu.Dropdown>
-							<Menu.Label>Application</Menu.Label>
-							<Menu.Item
-								leftSection={
-									<ThemeIcon variant="transparent">
-										<img src="/images/user.svg" alt="user" width="70%" />
-									</ThemeIcon>
-								}
-							>
-								Profile
-							</Menu.Item>
-							<Menu.Item
-								leftSection={
-									<ThemeIcon variant="transparent">
-										<img
-											src="/images/notifications.svg"
-											alt="notifications"
-											width="70%"
-										/>
-									</ThemeIcon>
-								}
-							>
-								Notifications
-							</Menu.Item>
-							<Menu.Item
-								leftSection={
-									<ThemeIcon variant="transparent">
-										<img src="/images/setting.svg" alt="setting" width="70%" />
-									</ThemeIcon>
-								}
-							>
-								Setting
-							</Menu.Item>
-							<Menu.Item
-								component="a"
-								href="/dashboard"
-								leftSection={
-									<ThemeIcon variant="transparent">
-										<img
-											src="/images/dashboard.svg"
-											alt="dashboard"
-											width="70%"
-										/>
-									</ThemeIcon>
-								}
-							>
-								Enter Dashboard
-							</Menu.Item>
-							<Menu.Divider />
+								<Menu.Label>Danger zone</Menu.Label>
 
-							<Menu.Label>Danger zone</Menu.Label>
-
-							<Menu.Item
-								color="red"
-								onClick={logout}
-								component="button"
-								type="button"
-								leftSection={
-									<ThemeIcon variant="transparent">
-										<img src="/images/logout.svg" alt="logout" width="70%" />
-									</ThemeIcon>
-								}
-							>
-								Logout
-							</Menu.Item>
-						</Menu.Dropdown>
-					</Menu>
-				</div>
-			) : (
-				<nav className="nav-section">
+								<Menu.Item
+									color="red"
+									onClick={logout}
+									component="button"
+									type="button"
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/logout.svg" alt="logout" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Logout
+								</Menu.Item>							</Menu.Dropdown>
+						</Menu>
+					</div>
+				) : (
 					<ul>
 						<li>
 							<NavLink to="/register#">Register</NavLink>
@@ -224,9 +205,112 @@ const Header = () => {
 							</Button>
 						</li>
 					</ul>
-				</nav>
-			)}
-		</header>
+				)}
+			</nav>
+			<nav className="mobile-nav-section">
+				<Menu
+					shadow="md"
+					width={200}
+					styles={{
+						dropdown: {
+							width: "100vw",
+							left: "0px",
+							position: "absolute"
+						},
+						itemLabel: {
+							fontSize: "1.25em"
+						}
+					}}
+					offset={0}
+				>
+					<Menu.Target>
+						<Button
+							variant="white"
+							color="gray"
+							leftSection={
+								<ThemeIcon variant="white" color="gray">
+									<Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />
+								</ThemeIcon>
+							}
+							styles={{
+								section: { margin: "0px", padding: "0 5px" },
+								root: {
+									padding: "0px",
+								},
+							}}
+						/>
+					</Menu.Target>
+
+					<Menu.Dropdown>
+						<Menu.Item component="a" href="/">
+							Home
+						</Menu.Item>
+						<Menu.Item component="a" href="/listing">
+							Listing
+						</Menu.Item>
+						<Menu.Item component="a" href="/faq">
+							FAQ
+						</Menu.Item>
+						<Menu.Item component="a" href="/about-us">
+							About Us
+						</Menu.Item>
+						<Menu.Divider />
+						{isCustomer ? (
+							<>
+								<Menu.Label>Application</Menu.Label>
+								<Menu.Item
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/user.svg" alt="user" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Profile
+								</Menu.Item>
+								<Menu.Item
+									component="a"
+									href="/dashboard"
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/dashboard.svg" alt="dashboard" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Enter Dashboard
+								</Menu.Item>
+								<Menu.Divider />
+
+								<Menu.Label>Danger zone</Menu.Label>
+
+								<Menu.Item
+									color="red"
+									onClick={logout}
+									component="button"
+									type="button"
+									leftSection={
+										<ThemeIcon variant="transparent">
+											<img src="/images/logout.svg" alt="logout" width="70%" />
+										</ThemeIcon>
+									}
+								>
+									Logout
+								</Menu.Item>
+							</>
+						) : (
+							<>
+								<Menu.Item component="a" href="/login">
+									Login
+								</Menu.Item>
+								<Menu.Item component="a" href="/register">
+									Register
+								</Menu.Item>
+							</>
+						)
+						}
+					</Menu.Dropdown>
+				</Menu>
+			</nav >
+		</header >
 	);
 	return (
 		<>
