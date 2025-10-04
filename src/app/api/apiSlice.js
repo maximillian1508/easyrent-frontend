8cad4a832bb7d9d8896054a191cd8478624e69e8
maxiPG1508 <maximillian@propertygenie.com.my>
2025-10-04 17:28:42 +0800

    build: proper dev/prod env separation

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: "http://localhost/api",
+	baseUrl: process.env.REACT_APP_API_URL || "http://localhost/api",
 	credentials: "include",
 	prepareHeaders: (headers, { getState }) => {
 		const token = getState().auth.token;
